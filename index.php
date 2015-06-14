db68e029f7a9bfdcdd7952c3ac57e0e80a63169e
Mirza Sadic <mirza.sadic@stu.ssst.edu.ba>
2015-06-14 23:30:36 +0200

    initialization

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+
+<body>
+    <h1><?php echo "php znaci bukvalno mos koristit bilo gdje" ?></h1>
+
+    <?php echo "bilo kad"; ?>
+    
+    <?php echo "I mos ga otvarat i zatvarat kad ti je cejf. Echo je php verzija system.out.println, samo sto ne printa u konzolu nego konkretno na dokument (html)" ?>
+    </body>
+</html>
